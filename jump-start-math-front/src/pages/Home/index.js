19c3ac508b3705eb34e65b8ad8663180e01c8fc9
LuLCotrim <lucaslcotrim@hotmail.com>
2021-06-28 14:00:26 -0300

    página home

--- jump-start-math-front/src/pages/Home/index.js
+++ jump-start-math-front/src/pages/Home/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>
-      <header className="App-header">
-        <h1>Hello JumpStart <br />  Math</h1>
-      </header>
+    <div className="divInicial">
+      <div className="container d-flex align-items-center divCont">
+        <div className="container">
+          <h1 className="title">JumpStart</h1>
+          <h2 className="slogan">A sua ferramenta para ensino rápido<br/>e prático em matemática</h2>
+          <button className="saibaMais">
+            SAIBA MAIS
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
