f705677609866ee7e420c828fcf256c6b6954061
Henrique Canella <henrique.mandelli@gmail.com>
2021-06-29 19:00:47 -0300

    remove route made for test

--- jump-start-math-front/src/routes.js
+++ jump-start-math-front/src/routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import Content from './pages/Content';
 
 /** TO DO: Implement protected routes... */
 const Router = () => {
@@ -12,7 +11,6 @@ const Router = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/contact" component={Contact} />
-        <Route exact path="/content" component={Content} />
       </Switch>
     </BrowserRouter>
   );
