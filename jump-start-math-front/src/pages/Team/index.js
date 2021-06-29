bcddda92445f328681cb707afbbff0f7067c6e94
joao vitor <joaovitor.carvalho@novahaus.com.br>
2021-06-29 19:16:47 -0300

    feat: Add Header and Footer to pages

--- jump-start-math-front/src/pages/Team/index.js
+++ jump-start-math-front/src/pages/Team/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Markdown from '../../components/UI/Markdown';
+import Header from '../../components/UI/Header';
+import Footer from '../../components/UI/Footer';
 
 const Team = () => {
   return (
     <>
+      <Header/>
       <Markdown fileName="team"/>
+      <Footer/>
     </>
   );
 }
