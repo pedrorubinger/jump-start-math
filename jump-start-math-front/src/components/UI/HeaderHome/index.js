889ecccb44339f0b354e7768d49a4b0aac2fb046
Henrique Canella <henrique.mandelli@gmail.com>
2021-07-13 18:03:36 -0300

    Add modal popup

--- jump-start-math-front/src/components/UI/HeaderHome/index.js
+++ jump-start-math-front/src/components/UI/HeaderHome/index.js
@@ -3,11 +3,14 @@ import { Container, NavList, StyledLink, Home } from './styles';
 
 function HeaderHome() {
   
-  const [logCadast, setLogCadast] = useState(false);
+  const [logCadast, setLogCadast] = useState(true);
   const [email, setEmail] = useState();
   const [senha, setSenha] = useState();
   const [nome, setNome] = useState();
   const [tipoUser, setTipoUser] = useState(0);
+
+  console.log('oi');
+  console.log(logCadast);
   
   function autenticar(){
 
@@ -28,36 +31,11 @@ function HeaderHome() {
       <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog">
           {
-            logCadast === false &&
 
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Entrar</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                <form className="form-signin mx-auto">
-                  <div className="form-label-group my-2">
-                      <input onChange={(e)=> setNome(e.target.value)} id="inputNome" className="form-control" placeholder="Nome"></input>
-                  </div>
-                  <div className="form-label-group my-2">
-                      <input onChange={(e)=> setEmail(e.target.value)} type="email" id="inputEmail" className="form-control" placeholder="Email"></input>
-                  </div>
-                  <div className="form-label-group my-2">
-                      <input onChange={(e)=> setSenha(e.target.value)} type="password" id="inputPassword" className="form-control" placeholder="Senha"></input>
-                  </div>
-                  <button className="btn btn-lg btn-primary btn-block loginBtn my-2" onClick={autenticar} type="button">Entrar</button>
-                  <div className="text-center">
-                      <p>Não possui Login?<button onClick={setLogCadast(1)} className="btn btn-link">Cadastre-se aqui!</button></p>
-                  </div>
-                </form>
-              </div>
-            </div>
-          }
-          {
-            logCadast === true &&
+            logCadast ?
 
-            <div className="modal-content">
+            (
+              <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title" id="exampleModalLabel">Cadastro</h5>
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -75,11 +53,17 @@ function HeaderHome() {
                   </div>
                   <button className="btn btn-lg btn-primary btn-block loginBtn my-2" onClick={autenticar} type="button">Entrar</button>
                   <div className="text-center">
-                      <p>Não possui Login?<button onClick={setLogCadast(0)} className="btn btn-link">Cadastre-se aqui!</button></p>
+                      <p>Não possui Login?<button onClick={() => setLogCadast(false)} className="btn btn-link">Cadastre-se aqui!</button></p>
                   </div>
                 </form>
               </div>
             </div>
+            )
+
+            :
+
+            <h1>oie</h1>
+            
           }
         </div>
       </div>
