542c239066e2a927b8df68bdad36c1a04bd61dc3
LuLCotrim <lucaslcotrim@hotmail.com>
2021-07-13 10:34:59 -0300

    adicionado modal de login e cadastro

--- jump-start-math-front/src/components/UI/HeaderHome/index.js
+++ jump-start-math-front/src/components/UI/HeaderHome/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import { Container, NavList, StyledLink, Home } from './styles';
 
 function HeaderHome() {
+  
+  const [logCadast, setLogCadast] = useState(0);
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
+  const [nome, setNome] = useState();
+  const [tipoUser, setTipoUser] = useState(0);
+  
+  function autenticar(){
+
+  }
+
   return (
     <Container>
       <nav>
@@ -11,9 +22,67 @@ function HeaderHome() {
           <StyledLink to="/technologies">O Projeto</StyledLink>
           <StyledLink to="/team">Equipe</StyledLink>
           <StyledLink to="/contact">Contato</StyledLink>
+          <StyledLink data-bs-toggle="modal" data-bs-target="#exampleModal">Entrar/Cadastrar</StyledLink>
         </NavList>
       </nav>
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          {
+            logCadast === 0 &&
+            
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Entrar</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <form className="form-signin mx-auto">
+                  <div className="form-label-group my-2">
+                      <input onChange={(e)=> setNome(e.target.value)} id="inputNome" className="form-control" placeholder="Nome"></input>
+                  </div>
+                  <div className="form-label-group my-2">
+                      <input onChange={(e)=> setEmail(e.target.value)} type="email" id="inputEmail" className="form-control" placeholder="Email"></input>
+                  </div>
+                  <div className="form-label-group my-2">
+                      <input onChange={(e)=> setSenha(e.target.value)} type="password" id="inputPassword" className="form-control" placeholder="Senha"></input>
+                  </div>
+                  <button className="btn btn-lg btn-primary btn-block loginBtn my-2" onClick={autenticar} type="button">Entrar</button>
+                  <div className="text-center">
+                      <p>Não possui Login?<button onClick={setLogCadast(1)} className="btn btn-link">Cadastre-se aqui!</button></p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          }
+          {
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Cadastro</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <form className="form-signin mx-auto">
+                  <div className="form-label-group my-2">
+                      <input onChange={(e)=> setNome(e.target.value)} id="inputNome" className="form-control" placeholder="Nome"></input>
+                  </div>
+                  <div className="form-label-group my-2">
+                      <input onChange={(e)=> setEmail(e.target.value)} type="email" id="inputEmail" className="form-control" placeholder="Email"></input>
+                  </div>
+                  <div className="form-label-group my-2">
+                      <input onChange={(e)=> setSenha(e.target.value)} type="password" id="inputPassword" className="form-control" placeholder="Senha"></input>
+                  </div>
+                  <button className="btn btn-lg btn-primary btn-block loginBtn my-2" onClick={autenticar} type="button">Entrar</button>
+                  <div className="text-center">
+                      <p>Não possui Login?<button onClick={setLogCadast(0)} className="btn btn-link">Cadastre-se aqui!</button></p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          }
+        </div>
+      </div>
     </Container>
+
   );
 }
 
