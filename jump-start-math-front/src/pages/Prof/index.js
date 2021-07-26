47ecde47039ddfce2e320c607dd77f8699f2a6f4
LuLCotrim <lucaslcotrim@hotmail.com>
2021-07-26 17:32:40 -0300

    Adicionado verificação de login na área restrita

--- jump-start-math-front/src/pages/Prof/index.js
+++ jump-start-math-front/src/pages/Prof/index.js
@@ -1,8 +1,19 @@
 import react from 'react'
 import Header from '../../components/UI/Header'
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
 
 export default function Prof(match){
+    
+    const userLogado = useSelector(state => state.usuario.usuarioLogado);
+
     return(
-        <Header/>
+        <>
+            <Header/>
+            {
+                userLogado < 0 ? <Redirect to="/"/> : null
+            }
+        </>
     )
 }
--- jump-start-math-front/src/pages/Student/index.js
+++ jump-start-math-front/src/pages/Student/index.js
@@ -1,8 +1,18 @@
 import react from 'react'
 import Header from '../../components/UI/Header'
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 export default function Student(match){
+    
+    const userLogado = useSelector(state => state.usuario.usuarioLogado);
+
     return(
-        <Header/>
+        <>
+            <Header/>
+            {
+                userLogado < 0 ? <Redirect to="/"/> : null
+            }
+        </>
     )
 }
