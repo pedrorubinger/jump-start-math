6535f0369eecf91f6c6d9701887f2b2874151ef1
Pedro Rubinger <pedro.rubinger@gmail.com>
2021-07-26 20:14:30 -0300

    fix: fixes reducer

--- jump-start-math-front/src/pages/Home/index.js
+++ jump-start-math-front/src/pages/Home/index.js
@@ -1,42 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 import "./home.css";
-import Markdown from '../../components/UI/Markdown';
 import HeaderHome from '../../components/UI/HeaderHome';
-import { Link, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 const Home = () => {
-
-  const userLogado = useSelector(state => state.usuario.usuarioLogado);
-  const userId = useSelector(state => state.usuario.usuarioId);
-  const tipoUser = useSelector(state => state.usuario.usuarioTipo);
-
-  console.log(typeof userLogado);
-
   return (
     <div className="divInicial">
-
-      {
-        userLogado > 0 ?   
-        tipoUser > 1 ?
-            <Redirect to={{
-              pathname:`/professor/${userId}`
-            }} />
-          : <Redirect to={{
-            pathname:`/aluno/${userId}`
-          }} />
-        : null
-      }
 
       <HeaderHome />
       <div className="container d-flex align-items-center divCont">
         <div className="container">
           <h1 className="title">JumpStart</h1>
-          <h2 className="slogan">A sua ferramenta para ensino rápido<br/>e prático em matemática</h2>
+          <h2 className="slogan">
+            A sua ferramenta para ensino rápido<br/>e prático em matemática
+          </h2>
           <Link className="saibaMais" to='/content'>
             SAIBA MAIS
           </Link>
-          <Markdown fileName="home"/>
         </div>
       </div>
     </div>
